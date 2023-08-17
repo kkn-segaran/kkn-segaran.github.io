2f4cc495b1b794d72a1c24d4cde6f8af06249dbc
devalltect00 <rizkyslenovo29@gmail.com>
2023-08-17 08:38:46 +0700

    modified and fill content

--- src/js/script.js
+++ src/js/script.js
@@ -4,56 +4,58 @@ const sliderNavigation = document.getElementById('sliderNavigation')
 let currentIndex = 0
 
 const forestPictureNames = [
-  'Mystical Forest',
-  'Enchanted Woods',
-  'Whispering Pines',
-  'Magic Glen',
+  'Candi Jiwa',
+  'Candi Blandongan',
+  'Candi Serut',
 ]
 
 const slideContent = [
   {
-    title: 'Mystical Forest',
+    title: 'Candi Jiwa',
     history:
-      "This is the history of the Mystical Forest. It's a place of magic and mystery.",
+      "Candi Jiwa merupakan sebuah gundukan tanah seperti bukit kecil yang biasa disebut penduduk unur jiwa berbentuk lonjong. Memiliki ketinggian 4 m dari permukaan tanah sawah di sekitarnya  dan luas sekitarnya 500 m². Pada tahun 1985 situs ini pertama kali diekskavasi dan dilanjutkan pada tahun 1986. Situs ini  semula digarap oleh penduduk sebagai lahan  pertanian yang ditanami pohon pisang dan  pala wija. Dari hasil penelitian kemungkinan besar candi jiwa merupakan sebuah candi berbentuk\xa0stupa.",
     gallery: [
       'https://via.placeholder.com/200',
       'https://via.placeholder.com/200',
       'https://via.placeholder.com/200',
     ],
     location: {
-      name: 'Mystical Forest',
-      latitude: 40.7128,
-      longitude: -74.006,
+      name: 'Candi Jiwa',
+      latitude: -6.05702,
+      longitude: 107.154689,
+      link: "https://www.google.co.id/maps/search/Candi+Jiwa/@-6.05702,107.1521141,17",
     },
   },
   {
-    title: 'Enchanted Woods',
+    title: 'Candi Blandongan',
     history:
-      'The Enchanted Woods are full of wonder and enchantment. Many mystical creatures reside here.',
+      'Situs ini pertama kali di survei oleh  tim arkeologi FSUI pada tahun 1984,  antara tahun 1992 dan 2000 situs ini di  eskapasi oleh Puslit Arkenas, dan  menghasilkan penemuan sebuah  reruntuhan candi dengan bagian kaki  yang berdenah bujur sangkar dengan  ukuran 25 x 25 m. Di beberapa tempat pada bagian kaki candi masih ditemukan lepa stuko yang menempel pada lapisan stuko tersebut, eskavasi oleh puslit arkenas pada tahun 1995 telah menemukan sebuah temuan  berupa fragmen meterai (votive tablet) terakota bergambar relief Budha.  Pada tahun berikutnya  telah di temukan pula pecahan votive tablet dalam jumlah yang lebih banyak, yaitu 5 buah utuhan dan  lebih dari 50 potong pecahannya. Beberapa pecahan tersebut masih dapat di utuhkan  kembali. Sebagian votive tablet tersebut ada yang berinskripsi.   Sejak tahun 1999 Suaka Peninggalan purbakala Sejarah dan Purbakala (sekarang: Balai Pelestarian  Peninggalan Purbakala) Serang. Telah melakukan pula berbagai kegiatan dalam rangka pemugaran  candi ini, di antaranya melakukan penggalian-pengupasan untuk mengangkat reruntuhan bata.  Keempat inskripsi tersebut berisi ayat–ayat suci agama Buddha, yang semuanya di  goreskan dengan aksara palawa dan bahasa sansekerta. Pada tahun 2002 dan 2003 Puslit Arkenas bersama EFEO kembali melakukan penelitian di SEG V.  Penelitian ini di peroleh data mengenai keadaan stratigrafi situs ini, khususnya pada sisi timur laut  kaki candi. Kemudian Balai Pelestarian Peninggalan Purbakala Serang melakukan eskapasi di halaman  candi di sisi timur laut dan tenggara, telah menemukan pula runtuhan pagar keliling dengan sisa-sisa  bagian pintu atau gapura di bagian tengah masing-masing sisi pagarkeliling\xa0tersebut.',
     gallery: [
       'https://via.placeholder.com/200',
       'https://via.placeholder.com/200',
       'https://via.placeholder.com/200',
     ],
     location: {
-      name: 'Enchanted Woods',
+      name: 'Candi Blandongan',
       latitude: 34.0522,
       longitude: -118.2437,
+      link: "https://www.google.co.id/maps/search/Candi+Blandongan/@-6.0556198,107.148646,17",
     },
   },
   {
-    title: 'Whispering Pines',
+    title: 'Candi Serut 1a',
     history:
-      'The Whispering Pines have a tale of ancient whispers and stories carried by the wind.',
+      'Sejarah Candi Serut',
     gallery: [
       'https://via.placeholder.com/200',
       'https://via.placeholder.com/200',
       'https://via.placeholder.com/200',
     ],
     location: {
-      name: 'Whispering Pines',
+      name: 'Candi Serut 1a',
       latitude: 51.5074,
       longitude: -0.1278,
+      link: "https://www.google.co.id/maps/search/Candi+serut+1a/@-6.0560039,107.1483191,17",
     },
   },
   {
@@ -122,26 +124,32 @@ function showSlide(index) {
   slideContentElement.innerHTML = `
     <h2>${slideContent[index].title}</h2>
     <hr>
-    <h3>History</h3>
+    <h3>Sejarah</h3>
     <p>${slideContent[index].history}</p>
     <hr>
-    <h3>Gallery</h3>
+    <h3>Galeri</h3>
     <div class="gallery">
       ${slideContent[index].gallery
         .map(imageUrl => `<img src="${imageUrl}" alt="Image">`)
         .join('')}
     </div>
     <hr>
-    <h3>Location</h3>
+    <h3>Lokasi</h3>
     <div class="location">
-      <a href="#" onclick="openLocationLink(${
-        slideContent[index].location.latitude
-      }, ${slideContent[index].location.longitude})">
+      <a href="#" onclick=openLocationLink("${
+        slideContent[index].location.link}")>
         ${slideContent[index].location.name}
       </a>
     </div>
     <hr>
   `
+
+  // <a href="#" onclick="openLocationLink(${
+  //   slideContent[index].location.latitude
+  // }, ${slideContent[index].location.longitude})">
+  //   ${slideContent[index].location.name}
+  // </a>
+  
 
   // Handle hiding/showing arrow buttons
   const leftArrowBtn = document.querySelector('.arrow-btn.left')
@@ -187,13 +195,19 @@ for (let i = 0; i < slides.length; i++) {
 
   // Set a random forest background image for each slide
   const randomImageUrl = `https://source.unsplash.com/1600x900/?forest,landscape&sig=${i}`
-  slides[i].style.backgroundImage = `url('${randomImageUrl}')`
+  // slides[i].style.backgroundImage = `url('${randomImageUrl}')`
+  slides[i].style.backgroundImage = `url('../../src/image/test.jpg')`
 }
 
-function openLocationLink(latitude, longitude) {
-  const url = `https://www.google.com/maps?q=${latitude},${longitude}`
-  window.open(url, '_blank')
+// function openLocationLink(latitude, longitude) {
+//   const url = `https://www.google.com/maps?q=${latitude},${longitude}`
+//   window.open(url, '_blank')
+// }
+
+function openLocationLink(urlLink) {
+  window.open(urlLink, '_blank')
 }
 
 // Show the initial slide and mark the first circle as active
 showSlide(currentIndex)
+
